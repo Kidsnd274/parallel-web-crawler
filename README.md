f6b9f52033d20af74fccd4918d40ca67a6f282d9
Kidsnd274 <samuel_tee_gy@hotmail.com>
2023-10-30 18:09:05 +0800

    Added more crawler stuff

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # parallel-web-crawler
-To run this program, run the command
+To run this program, first setup the environment (Windows commands)
 ```
+python -m venv venv
+.\venv\Scripts\activate.bat
+pip install -r requirements.txt
 python main.py <url>
+```
+
+To test crawler with one url, run the following command:
+```
+python crawler.py https://google.com
 ```
--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import urllib.parse
 import requests
+import socket
 from bs4 import BeautifulSoup
 
 class CrawlInfo:
@@ -10,17 +11,19 @@ class CrawlInfo:
         self.html = html
         self.url_list = url_list
         
+    def __str__(self):
+        return str(self.url_list)
+    
     def get_info(self):
         return (self.ip_address, self.response_time, self.geolocation)
     
 class Crawler: # Takes in one URL and returns a list of URLs in that page
     def __init__(self, url):
         self.url = url
-        self.url_list = []
         self.crawl_info = None
     
     def start_crawling(self):
-        if self.crawl_info is None:
+        if self.crawl_info is not None:
             raise Exception # This Crawler has already been ran
         
         # Download html
@@ -32,17 +35,27 @@ class Crawler: # Takes in one URL and returns a list of URLs in that page
         html = r.text
         
         # Extract urls
+        url_list = []
         soup = BeautifulSoup(html, 'html.parser')
         for link in soup.find_all('a'):
             href_url = link.get('href')
             if href_url and href_url.startswith('/'):
                 href_url = urllib.parse.urljoin(self.url, href_url)
-            self.url_list.append(href_url)
+            url_list.append(href_url)
         
-        return self.get_url_list()
+        # TODO: NOT WORKING
+        # Extract IP address, response time and geolocation
+        # socket = r.raw._connection.sock
+        # ip_address, port = socket.getpeername()
+        response_time = r.elapsed.total_seconds()
+        geolocation = None
+
+        results = CrawlInfo(ip_addresss=None, response_time=response_time, geolocation=geolocation, html=html, url_list=url_list)
+        self.crawl_info = results
+        return results
     
     def get_url_list(self):
-        return self.url_list
+        return self.crawl_info.url_list
 
 
 if __name__ == "__main__":
@@ -52,5 +65,12 @@ if __name__ == "__main__":
     except IndexError:
         print("Please input an URL!")
     test_crawler = Crawler(url)
-    print(test_crawler.start_crawling())
+    test_crawler.start_crawling()
+
+    results = test_crawler.crawl_info
+
+    print(f"IP Address: {str(results.ip_address)}")
+    print(f"Response Time: {str(results.response_time)}s")
+    print(f"Geolocation: {str(results.geolocation)}")
+    print(f"URLS: {str(results.url_list)}")
     
